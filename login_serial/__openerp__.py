63f1c7c26d554e8d3382aa07a385c877ceb58bd3
Damian Soriano <damiansoriano@gmail.com>
2014-11-11 22:27:08 +0000

    Add description in __openerp__.py file.

--- login_serial/__openerp__.py
+++ login_serial/__openerp__.py
@@ -3,7 +3,11 @@
     'category': 'Login Serial',
     'summary': 'Login Serial',
     'version': '1.0',
-    'description': 'Login Serial',
+    'description':
+        '''
+    Adds a Serial Id field to the users that can be used to login in without a password.
+
+    A new URL is provided "http://domain/login_serial/" to login with the specified Serial Id.''',
     'author': 'Damian Soriano <ds@ingadhoc.com>',
     'depends': ['website'],
     'external_dependencies': {
