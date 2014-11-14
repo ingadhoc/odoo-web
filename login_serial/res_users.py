f7fd94853d7f1ad7560de92eb00c3ba4324358b1
Damian Soriano <damiansoriano@gmail.com>
2014-11-14 22:56:50 +0000

    When create a fresh Serial Id is generated.
    Add button go generate fresh Serial Id.

--- login_serial/res_users.py
+++ login_serial/res_users.py
@@ -1,7 +1,19 @@
 import logging
+import random
 
 from openerp import SUPERUSER_ID
+from openerp.addons.product.product import check_ean, sanitize_ean13
 from openerp.osv import fields, osv
+
+
+MAX_RETRY_FOR_GENERATE_SERIAL_ID = 10000
+
+
+def generate_serial_id():
+    serial_id = ''
+    for _ in range(12):
+        serial_id += str(int(random.random() * 10))
+    return sanitize_ean13(serial_id)
 
 
 class res_users(osv.osv):
@@ -26,3 +38,46 @@ class res_users(osv.osv):
         if res:
             return res
         super(res_users, self).check_credentials(cr, uid, password)
+
+    def _validate_serial_id(self, vals):
+        if 'serial_id' in vals and not check_ean(vals['serial_id']):
+            raise osv.except_osv(
+                'Serial Id invalid format',
+                'The Serial Id field has not the EAN-13 format standard.'
+            )
+
+    def _get_fresh_serial_id(self, cr, uid, context=None):
+        for _ in range(MAX_RETRY_FOR_GENERATE_SERIAL_ID):
+            serial_id = generate_serial_id()
+            if not self.search(
+                cr, uid, [('serial_id', '=', serial_id)], context=context
+            ):
+                return serial_id
+        raise osv.except_osv(
+            'Cannot generate Serial Id',
+            'Odoo was unable to generate a fresh random Serial Id.' +
+            ' It may be that there are a big amount of Serial Id' +
+            ' already generated. You can try again.'
+        )
+
+    def create(self, cr, uid, vals, context=None):
+        if 'serial_id' in vals:
+            self._validate_serial_id(vals)
+        else:
+            vals['serial_id'] = self._get_fresh_serial_id(
+                cr, uid, context=context
+            )
+        return super(res_users, self).create(cr, uid, vals, context=context)
+
+    def write(self, cr, uid, ids, vals, context=None):
+        self._validate_serial_id(vals)
+        return super(res_users, self).write(
+            cr, uid, ids, vals, context=context
+        )
+
+    def generate_fresh_serial_ids(self, cr, uid, ids, context=None):
+        for id in ids:
+            serial_id = self._get_fresh_serial_id(cr, uid, context=context)
+            self.write(
+                cr, uid, id, {'serial_id': serial_id}, context=context
+            )
--- login_serial/tests/test_serial_login.py
+++ login_serial/tests/test_serial_login.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import logging
 
 from openerp.addons.product.product import check_ean
+from openerp.osv import fields, osv
+from openerp.addons.login_serial.res_users import generate_serial_id
 from openerp.tests.common import TransactionCase
 
 
@@ -48,6 +50,22 @@ class TestSerialLogin(TransactionCase):
                 self.cr, self.uid, new_user_id, {'serial_id': serial_id}
             )
 
+    def test_serial_id_should_be_valid_ean13(self):
+        name = self.get_new_user_name()
+        invalid_serial_id = '5012345678901'
+        serial_id = '5012345678900'
+
+        with self.assertRaises(osv.except_osv):
+            self.create_user(
+                name=name, login=name, serial_id=invalid_serial_id
+            )
+
+        user_id = self.create_user(name=name, login=name, serial_id=serial_id)
+        with self.assertRaises(osv.except_osv):
+            self.user_pool.write(
+                self.cr, self.uid, user_id, {'serial_id': invalid_serial_id}
+            )
+
     def test_create_generates_valid_ean(self):
         name = self.get_new_user_name()
         user_id = self.create_user(name=name, login=name)
@@ -58,3 +76,37 @@ class TestSerialLogin(TransactionCase):
             'The user\'s Serial Id is not a valid EAN-13 code.'
         )
 
+    def test_generate_serial_id_are_valid_ean(self):
+        for _ in range(1000):
+            serial_id = generate_serial_id()
+            self.assertTrue(
+                check_ean(serial_id),
+                'The generate Serial Id "{0}" is not a valid EAN-13'.format(
+                    serial_id
+                )
+            )
+
+    def test_generate_fresh_serial_ids(self):
+        name_1 = self.get_new_user_name()
+        user_id_1 = self.create_user(name=name_1, login=name_1)
+
+        name_2 = self.get_new_user_name()
+        user_id_2 = self.create_user(name=name_2, login=name_2)
+
+        self.user_pool.write(
+            self.cr, self.uid, [user_id_1, user_id_2], {'serial_id': None}
+        )
+        self.user_pool.generate_fresh_serial_ids(
+            self.cr, self.uid, [user_id_1, user_id_2]
+        )
+
+        user_1 = self.user_pool.browse(self.cr, self.uid, user_id_1)
+        user_2 = self.user_pool.browse(self.cr, self.uid, user_id_2)
+        for user in [user_1, user_2]:
+            self.assertTrue(user.serial_id)
+            self.assertTrue(
+                check_ean(user.serial_id),
+                'The generate Serial Id "{0}" is not a valid EAN-13'.format(
+                    user.serial_id
+                )
+            )
