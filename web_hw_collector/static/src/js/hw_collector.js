2f30ea89f775c2be4e544ac26d878d5a4ed82cdc
Damian Soriano <damiansoriano@gmail.com>
2015-02-24 23:02:55 +0000

    Add event on button click

--- web_hw_collector/static/src/js/hw_collector.js
+++ web_hw_collector/static/src/js/hw_collector.js
@@ -2,7 +2,10 @@ openerp.web_hw_collector = function(instance) {
     instance.web_hw_collector.HwCollector = instance.web.form.FieldChar.extend({
         template: "HwCollector",
         start: function() {
-            this.$el.append("<div>Hello dear Odoo user!</div>");
+            var _self = self;
+            self.$(".hw_collector_button").click(function() {
+                alert('a');
+            });
         },
     });
 
