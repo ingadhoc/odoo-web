5827f882641c2886dfcbd5e67c4d67f2c023a9dd
Damian Soriano <damiansoriano@gmail.com>
2015-04-04 16:52:39 +0100

    Render and save dummy data to measured_weight

--- web_hw_collector/static/src/js/hw_collector.js
+++ web_hw_collector/static/src/js/hw_collector.js
@@ -2,9 +2,12 @@ openerp.web_hw_collector = function(instance) {
     instance.web_hw_collector.HwCollector = instance.web.form.FieldChar.extend({
         template: "HwCollector",
         start: function() {
-            var _self = self;
+            var _this = this;
+            this.render_value();
             self.$(".hw_collector_button").click(function() {
-                alert('a');
+                _this.set_value('9');
+                _this.render_value();
+                _this.view.save();
             });
         },
     });
