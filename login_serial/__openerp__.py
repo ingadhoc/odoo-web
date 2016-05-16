729547bf9ba40f6498503d2416872bc4014561f0
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2016-05-16 10:42:09 -0300

    FIX instalable to login_serial

--- login_serial/__openerp__.py
+++ login_serial/__openerp__.py
@@ -21,6 +21,6 @@ Serial Id.''',
         'views/res_users_view.xml',
         'views/webclient_templates.xml',
     ],
-    'installable': False,
+    'installable': True,
     # TODO fix, it gives an error on runbot
 }
