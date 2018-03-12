eb60eebb2c33a3b853c3ae4f006a52c35b39c3aa
Juan José Scarafía <jjs@adhoc.com.ar>
2018-03-12 19:17:31 -0300

    [REM] login_serial deprecated for v11 (#20)

--- login_serial/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import res_partner
-from . import res_users
-from . import controllers
-from . import functions
--- login_serial/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# from . import test_serial_login
-
-# checks = [
-#     test_serial_login,
-# ]
